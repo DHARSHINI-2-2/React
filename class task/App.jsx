d721530c91dfc9542c57fad68200cd738f752ab3
DHARSHINI-2-2 <smdharshini22@gmail.com>
2025-12-30 10:50:30 +0530

    Add files via upload

---
+++ class task/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div style={{width:400}}>
+          <p class="bg-amber-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam laborum autem fugiat aliquam optio ducimus qui ipsam cum voluptatum quae sapiente, nam a dignissimos quaerat quis excepturi nihil ad ullam voluptas deserunt possimus ab perspiciatis repellendus. Omnis ea magnam, quo, soluta repellendus qui perferendis molestias corrupti veritatis ipsam officia iusto.</p><br />
+          <p class="bg-emerald-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti nostrum alias dignissimos pariatur consectetur temporibus sed id maxime omnis quos.</p><br />
+          <p style={{background:"blue"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, consequatur?</p>
+          <p></p>
+          <p></p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
